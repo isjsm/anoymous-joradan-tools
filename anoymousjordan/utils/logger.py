2d47a1cfbaf7dd5d70b5a12d5c746225d6d69c58
isjsm <84210598+isjsm@users.noreply.github.com>
2025-03-09 14:29:07 +0300

    Update logger.py

--- anoymousjordan/utils/logger.py
+++ anoymousjordan/utils/logger.py
@@ -1,1 +1,5 @@
-
+# في utils/logger.py
+def secure_log(data):
+    encrypted = AESEncryptor().encrypt(str(data))
+    with open('/var/log/anoymousjordan/secure.log', 'a') as f:
+        f.write(f"{encrypted}\n")
