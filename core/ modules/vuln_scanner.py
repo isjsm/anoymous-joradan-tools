dda50a77c6a70a996502a2b9715cf1afaab3c723
isjsm <84210598+isjsm@users.noreply.github.com>
2025-03-09 13:39:57 +0300

    Update vuln_scanner.py

--- core/ modules/vuln_scanner.py
+++ core/ modules/vuln_scanner.py
@@ -1,1 +1,13 @@
+# modules/vuln_scanner.py
+import requests
 
+class VulnerabilityScanner:
+    def scan_sql_injection(self, url):
+        # محاكاة فحص SQL Injection
+        try:
+            payload = "' OR 1=1--"
+            response = requests.get(f"{url}?id={payload}")
+            if "error" in response.text.lower():
+                print(f"[!] SQL Injection vulnerability detected in {url}")
+        except:
+            print("[-] Connection error")
